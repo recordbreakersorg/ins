0ed43e2a9c9a164e25eccc04ee40b1e636c3aac6
ken-morel <engonken8@gmail.com>
2025-06-20 21:28:58 +0100

    Wrapped app in animation builder for theme autoswitch and hotreload on theme chanje

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:ins/theme.dart';
-import 'package:ins/pages/home.dart';
+import 'package:ins/pages/home.dart' as home;
 
 class ISApp extends StatelessWidget {
   const ISApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'IS',
-      theme: themeManager.getTheme(),
-      home: getPage(),
+    return AnimatedBuilder(
+      animation: themeManager,
+      builder: (_, _) {
+        return MaterialApp(
+          title: 'IS',
+          theme: themeManager.getTheme(),
+          home: home.getPage(),
+        );
+      },
     );
   }
 }
