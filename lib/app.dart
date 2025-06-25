6aaba3d2ce9db425c59731ee9a4f8813265aafde
ken-morel <engonken8@gmail.com>
2025-06-25 13:54:13 +0100

    Added automatic language switching for flutter widgets

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ins/theme.dart';
 import 'package:ins/pages/home.dart' as home;
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class ISApp extends StatelessWidget {
   const ISApp({super.key});
@@ -12,6 +13,12 @@ class ISApp extends StatelessWidget {
       builder: (_, _) {
         return MaterialApp(
           title: 'IS',
+          localizationsDelegates: [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: [Locale('en'), Locale('fr')],
           theme: themeManager.getTheme(),
           home: home.getPage(),
         );
