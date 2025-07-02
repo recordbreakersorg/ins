3e7e315b44f52fbe15eb146cb9d9dafc0d5e1926
ken-morel <engonken8@gmail.com>
2025-07-03 00:18:16 +0100

    updated android package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "cm.br.ins"
+    namespace = "cm.rbs.ins"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "29.0.13599879"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "cm.br.ins"
+        applicationId = "cm.rbs.ins"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
