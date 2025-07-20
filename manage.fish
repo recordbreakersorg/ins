0bef3f7d7874fb2d2004d46aa5f2315835b3cd4e
ken-morel <engonken8@gmail.com>
2025-07-20 01:09:30 +0100

    added manage.fish

---
+++ manage.fish
@@ -0,0 +1,23 @@
+#!/usr/bin/fish
+
+function deploy
+    echo "[manage.fish] Deploying on firebase "
+    firebase deploy
+end
+function build-web
+    echo "[manage.fish] Building for the web"
+    flutter build web --no-web-resources-cdn --optimization-level 4
+end
+
+set usage "manage deploy|build-web"
+
+if test $argv[1] = deploy
+    deploy
+else if test $argv[1] = build-web
+    build-web
+else if test $argv[1] = fire
+    build-web
+    deploy
+else
+    echo $usage
+end
